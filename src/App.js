dfefba8569eca7791065901cade034d1ef118fed
max <maxjunhuixu@gmail.com>
2020-03-18 18:46:00 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+/* global GLOBAL_CONFIG */
+
 import React, { useReducer } from 'react'
 import './App.css'
 import { AppContext, AppReducer, initialState } from './AppReducer'
 
 const App = () => {
+  console.log(GLOBAL_CONFIG)
   const [state, dispatch] = useReducer(AppReducer, initialState)
   return (
     <AppContext.Provider value={ { state, dispatch } }>
